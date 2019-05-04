4c3f09fe6b533c9edbf22b15349a4d842466fcd6
DIRCEU_OTAVIO_VIERA_BELEM <dirceu@ALFAB52.BARROCA>
2019-05-03 21:20:43 -0300

    Descomentando Serviço

--- src/main/java/br/com/task/api/ApplicationConfig.java
+++ src/main/java/br/com/task/api/ApplicationConfig.java
@@ -25,7 +25,7 @@ public class ApplicationConfig extends Application {
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
 
-        // resources.add(ServiceAccount.class);
+         resources.add(ServiceUser.class);
 
     }
 
