2db6c3b7f9cb22ccbbb64d17593f4d244eb2f7df
Dirceu Belém <dirceu@fourtime.com>
2019-05-06 14:19:02 -0300

    Criando serviço de versão

---
+++ src/main/java/br/com/task/api/ServiceVersion.java
@@ -0,0 +1,14 @@
+package br.com.task.api;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("version")
+public class ServiceVersion {
+
+    @GET
+    public String getVersion() {
+        return "1.0";
+    }
+
+}
