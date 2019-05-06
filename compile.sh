2d31ea1f5e76426245a52cef0bf53cdda8ba694c
Dirceu Belém <dirceu@fourtime.com>
2019-05-06 18:36:02 -0300

    Ajustando pasta

--- compile.sh
+++ compile.sh
@@ -2,4 +2,4 @@
 rm -rf /opt/tomcat/webapps/br.com.task.api &&
 rm -r /opt/tomcat/webapps/br.com.task.api.war &&
 cp build/libs/*.war /opt/tomcat/webapps
-sudo -u postgres /usr/lib/postgresql/9.6/bin/./psql fluo < '/home/admin/task/task-api/src/main/java/br.com.task/dao/script.sql'
+sudo -u postgres /usr/lib/postgresql/9.6/bin/./psql fluo < '/home/admin/task/task-api/src/main/java/br/com/task/dao/script.sql'
