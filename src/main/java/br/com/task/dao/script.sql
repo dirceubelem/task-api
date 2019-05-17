aca86c2b2d62c4904faf23a814e9ae2606092873
DIRCEU_OTAVIO_VIERA_BELEM <dirceu@ALFAB52.BARROCA>
2019-05-17 19:47:55 -0300

    Criando tarefa e status da tarefa

--- src/main/java/br/com/task/dao/script.sql
+++ src/main/java/br/com/task/dao/script.sql
@@ -1,3 +1,5 @@
+--drop table status;
+--drop table task;
 drop table account;
 
 create table account (
@@ -14,3 +16,33 @@ create table account (
 alter table account add constraint pk_account primary key (id);
 
 insert into account values ('5C7M3JEI468RO1D9VVV2U2', 'Dirceu Belem', 'dirceubelem@gmail.com', '2e6f9b0d5885b6010f9167787445617f553a735f', null, true, now(), null);
+
+create table status (
+	id int not null,
+    name varchar(40) not null,
+    constraint pk_status primary key (id)
+);
+
+insert into status values (1, 'Backlog');
+insert into status values (2, 'In progress');
+insert into status values (3, 'Test');
+insert into status values (4, 'Done');
+
+create table task (
+	id varchar(22) not null,
+  idstatus int not null,
+  idaccountfrom varchar(22) not null,
+  idaccountto varchar(22) not null,
+  name varchar(300) not null,
+	description varchar(5000),
+  tags varchar(300),
+  estimate double, -- hours
+  createdat timestamp not null,
+  startedat timestamp,
+  deliveredat timestamp,
+  priority int,
+	constraint pk_task primary key (id),
+  constraint fk_task_status foreign key (idstatus) references status (id),
+  constraint fk_task_account_01 foreign key (idaccountto) references account (id),
+  constraint fk_task_account_02 foreign key (idaccountfrom) references account (id)
+);
---
+++ src/main/java/br/com/task/to/TOTask.java
@@ -0,0 +1,115 @@
+package br.com.task.to;
+
+import java.sql.Timestamp;
+
+public class TOTask {
+
+    private String id;
+    private int idStatus;
+    private String idAccountFrom;
+    private String idAccountTo;
+    private String name;
+    private String description;
+    private String tags;
+    private double estimate;
+    private Timestamp createdAt;
+    private Timestamp startedAt;
+    private Timestamp deliveredAt;
+    private int priority;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getIdStatus() {
+        return idStatus;
+    }
+
+    public void setIdStatus(int idStatus) {
+        this.idStatus = idStatus;
+    }
+
+    public String getIdAccountFrom() {
+        return idAccountFrom;
+    }
+
+    public void setIdAccountFrom(String idAccountFrom) {
+        this.idAccountFrom = idAccountFrom;
+    }
+
+    public String getIdAccountTo() {
+        return idAccountTo;
+    }
+
+    public void setIdAccountTo(String idAccountTo) {
+        this.idAccountTo = idAccountTo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+
+    public double getEstimate() {
+        return estimate;
+    }
+
+    public void setEstimate(double estimate) {
+        this.estimate = estimate;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getStartedAt() {
+        return startedAt;
+    }
+
+    public void setStartedAt(Timestamp startedAt) {
+        this.startedAt = startedAt;
+    }
+
+    public Timestamp getDeliveredAt() {
+        return deliveredAt;
+    }
+
+    public void setDeliveredAt(Timestamp deliveredAt) {
+        this.deliveredAt = deliveredAt;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+}
