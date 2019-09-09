67ec243015a0c23af49df7f6da3d1c90f9e4a3e8
Dirceu Belem <dirceu@fourtime.com>
2019-09-09 18:45:09 -0300

    Ajustando url

--- src/main/java/br/com/task/fw/Data.java
+++ src/main/java/br/com/task/fw/Data.java
@@ -19,8 +19,7 @@ import java.util.List;
 public class Data {
 
     public static Connection openConnection() throws Exception {
-//        return openConnectionMySql("localhost", "task", "root", "");
-        return openConnectionPostgre("api.fluo.site", "fluo", "fluo", "Senha123");
+        return openConnectionPostgre("localhost", "fluo", "fluo", "Senha123");
     }
 
     public static Connection openConnectionMySql(String server, String database, String user, String password) throws Exception {
